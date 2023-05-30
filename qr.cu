b4da2f97039c0f14f09565a7ca8dd4d3332356b7
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2023-05-30 13:47:22 -0400

    add print for debugging

--- qr.cu
+++ qr.cu
@@ -4,17 +4,6 @@
 #include <cublas_v2.h>
 #include <curand.h>
 
-// Function to generate a random matrix
-void generateRandomMatrix(float* matrix, int rows, int cols) {
-    curandGenerator_t prng;
-    curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
-    curandSetPseudoRandomGeneratorSeed(prng, time(NULL));  // Use current time as the seed
-    curandGenerateUniform(prng, matrix, rows * cols);
-    curandDestroyGenerator(prng);
-
-    printMatrix(matrix, rows, cols);
-}
-
 // Function to print a matrix
 void printMatrix(float *matrix, int rows, int cols)
 {
@@ -29,33 +18,50 @@ void printMatrix(float *matrix, int rows, int cols)
     printf("\n");
 }
 
+// Function to generate a random matrix
+void generateRandomMatrix(float *matrix, int rows, int cols)
+{
+    curandGenerator_t prng;
+    curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(prng, time(NULL)); // Use current time as the seed
+    curandGenerateUniform(prng, matrix, rows * cols);
+    curandDestroyGenerator(prng);
+
+    printMatrix(matrix, rows, cols);
+}
+
 // QR factorization kernel
-__global__ void qrFactorization(float* matrix, int rows, int cols, int k) {
+__global__ void qrFactorization(float *matrix, int rows, int cols, int k)
+{
     int i = blockIdx.y * blockDim.y + threadIdx.y;
     int j = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (i < rows && j < cols && j >= k) {
+    if (i < rows && j < cols && j >= k)
+    {
         float sum = 0.0f;
-        for (int l = k; l < rows; l++) {  // Update loop bounds from k to rows
+        for (int l = k; l < rows; l++)
+        { // Update loop bounds from k to rows
             sum += matrix[l * cols + k] * matrix[l * cols + j];
         }
 
         __syncthreads();
 
-        for (int l = k; l < rows; l++) {  // Update loop bounds from k to rows
+        for (int l = k; l < rows; l++)
+        { // Update loop bounds from k to rows
             matrix[l * cols + j] -= (matrix[l * cols + k] * sum);
         }
     }
 }
 
-int main() {
+int main()
+{
     int rows = 4;
     int cols = 3;
     int min_dim = (rows < cols) ? rows : cols;
     int matrix_size = rows * cols * sizeof(float);
 
     // Allocate memory for the matrix on the host
-    float* host_matrix = (float*)malloc(matrix_size);
+    float *host_matrix = (float *)malloc(matrix_size);
 
     // Generate a random matrix
     generateRandomMatrix(host_matrix, rows, cols);
@@ -64,8 +70,8 @@ int main() {
     printMatrix(host_matrix, rows, cols);
 
     // Allocate memory for the matrix on the device
-    float* device_matrix;
-    cudaMalloc((void**)&device_matrix, matrix_size);
+    float *device_matrix;
+    cudaMalloc((void **)&device_matrix, matrix_size);
 
     // Copy the matrix from host to device
     cudaMemcpy(device_matrix, host_matrix, matrix_size, cudaMemcpyHostToDevice);
@@ -75,21 +81,26 @@ int main() {
     cublasCreate(&handle);
 
     // Perform QR factorization
-    for (int k = 0; k < min_dim; k++) {
+    for (int k = 0; k < min_dim; k++)
+    {
         dim3 blockSize(32, 32);
         dim3 gridSize((cols + blockSize.x - 1) / blockSize.x, (rows + blockSize.y - 1) / blockSize.y);
 
         qrFactorization<<<gridSize, blockSize>>>(device_matrix, rows, cols, k);
 
         // Update Q matrix (optional)
-        if (k < rows - 1) {
-            float* column = device_matrix + k * cols + k;
+        if (k < rows - 1)
+        {
+            float *column = device_matrix + k * cols + k;
             int stride = 1;
             float norm;
             cublasSnrm2(handle, rows - k, column, stride, &norm);
-            if (column[0] >= 0) {
+            if (column[0] >= 0)
+            {
                 column[0] += norm;
-            } else {
+            }
+            else
+            {
                 column[0] -= norm;
             }
             float alpha = 1.0f / cublasSnrm2(handle, rows - k, column, stride, NULL);
