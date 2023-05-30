c65dd56753c353d7dce5ada6d90592ef09fab4a5
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2023-05-30 13:26:11 -0400

    Add initial qr code

---
+++ qr.cu
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <curand.h>
+
+// Function to generate a random matrix
+void generateRandomMatrix(float *matrix, int rows, int cols)
+{
+    curandGenerator_t prng;
+    curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(prng, 123456789);
+    curandGenerateUniform(prng, matrix, rows * cols);
+    curandDestroyGenerator(prng);
+}
+
+// Function to print a matrix
+void printMatrix(float *matrix, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%.2f ", matrix[i * cols + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// QR factorization kernel
+__global__ void qrFactorization(float *matrix, int rows, int cols, int k)
+{
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < rows && j < cols && j >= k)
+    {
+        float sum = 0.0f;
+        for (int l = 0; l < rows; l++)
+        {
+            sum += matrix[l * cols + k] * matrix[l * cols + j];
+        }
+
+        __syncthreads();
+
+        for (int l = 0; l < rows; l++)
+        {
+            matrix[l * cols + j] -= (matrix[l * cols + k] * sum);
+        }
+    }
+}
+
+int main()
+{
+    int rows = 4;
+    int cols = 3;
+    int min_dim = (rows < cols) ? rows : cols;
+    int matrix_size = rows * cols * sizeof(float);
+
+    // Allocate memory for the matrix on the host
+    float *host_matrix = (float *)malloc(matrix_size);
+
+    // Generate a random matrix
+    generateRandomMatrix(host_matrix, rows, cols);
+
+    printf("Input matrix:\n");
+    printMatrix(host_matrix, rows, cols);
+
+    // Allocate memory for the matrix on the device
+    float *device_matrix;
+    cudaMalloc((void **)&device_matrix, matrix_size);
+
+    // Copy the matrix from host to device
+    cudaMemcpy(device_matrix, host_matrix, matrix_size, cudaMemcpyHostToDevice);
+
+    // Create cuBLAS handle
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    // Perform QR factorization
+    for (int k = 0; k < min_dim; k++)
+    {
+        dim3 blockSize(32, 32);
+        dim3 gridSize((cols + blockSize.x - 1) / blockSize.x, (rows + blockSize.y - 1) / blockSize.y);
+
+        qrFactorization<<<gridSize, blockSize>>>(device_matrix, rows, cols, k);
+
+        // Update Q matrix (optional)
+        if (k < rows - 1)
+        {
+            float *column = device_matrix + k * cols + k;
+            float norm;
+            cublasSnrm2(handle, rows - k, column, 1, &norm);
+            if (column[0] >= 0)
+            {
+                column[0] += norm;
+            }
+            else
+            {
+                column[0] -= norm;
+            }
+            float alpha = 1.0f / cublasSnrm2(handle, rows - k, column, 1);
+            cublasSscal(handle, rows - k, &alpha, column, 1);
+        }
+    }
+
+    // Copy the result back to the host
+    cudaMemcpy(host_matrix, device_matrix, matrix_size, cudaMemcpyDeviceToHost);
+
+    printf("Q matrix:\n");
+    printMatrix(host_matrix, rows, cols);
+
+    // Clean up
+    cudaFree(device_matrix);
+    cublasDestroy(handle);
+    free(host_matrix);
+
+    return 0;
+}
